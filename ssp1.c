fe1ea890fb901a69b8ba584a83485ac7ea2ae2a3
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-02-02 13:56:03 +0530

    ssp1.c

---
+++ ssp1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+charn a;
+printf("enter the character");
+scanf("%c",&a);
+if(a>='a'&&a<='z'||a>='A'&&a<='z')
+print("It is an Alphabet");
+else
+printf("Not an Alphabet");
+getch();
+return 0;
+}
