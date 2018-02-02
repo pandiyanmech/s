1b160d3bbab04e11f1057198686fd0f7955cd5f8
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-02-02 14:09:10 +0530

    ssp3.c

---
+++ ssp3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int  main()
+{
+char a;
+printf("enter the letter\n");
+scanf("%c",&a);
+if((a>='a')&&(a<='z'))
+printf("enter letter is alphabet");
+else
+printf("enter letter is not alphabet");
+return 0;
+}
